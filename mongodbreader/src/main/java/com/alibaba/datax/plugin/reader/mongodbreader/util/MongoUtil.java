a361e11c47303df28a869ac8c6a4aadbf0dfad0c
beat4ocean <105700600+beat4ocean@users.noreply.github.com>
2024-04-26 10:25:51 +0800

    可用版

--- mongodbreader/src/main/java/com/alibaba/datax/plugin/reader/mongodbreader/util/MongoUtil.java
+++ mongodbreader/src/main/java/com/alibaba/datax/plugin/reader/mongodbreader/util/MongoUtil.java
@@ -1,63 +1,78 @@
 package com.alibaba.datax.plugin.reader.mongodbreader.util;
 
-import java.net.UnknownHostException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import com.alibaba.datax.common.exception.DataXException;
 import com.alibaba.datax.common.util.Configuration;
 import com.alibaba.datax.plugin.reader.mongodbreader.KeyConstant;
 import com.alibaba.datax.plugin.reader.mongodbreader.MongoDBReaderErrorCode;
-
+import com.mongodb.*;
 import com.mongodb.MongoClient;
-import com.mongodb.MongoCredential;
-import com.mongodb.ServerAddress;
 
-/**
- * Created by jianying.wcj on 2015/3/17 0017.
- * Modified by mingyan.zc on 2016/6/13.
- */
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+import org.bson.Document;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class MongoUtil {
+    private static final Logger LOG = LoggerFactory.getLogger(MongoUtil.class);
 
     public static MongoClient initMongoClient(Configuration conf) {
+        List addressList = conf.getList(KeyConstant.MONGO_ADDRESS);
 
-        List<Object> addressList = conf.getList(KeyConstant.MONGO_ADDRESS);
-        if (addressList == null || addressList.size() <= 0) {
+        if (addressList != null && !addressList.isEmpty()) {
+            try {
+                return new MongoClient(parseServerAddress(addressList));
+            } catch (UnknownHostException var3) {
+                throw DataXException.asDataXException(MongoDBReaderErrorCode.ILLEGAL_ADDRESS, "不合法的地址");
+            } catch (NumberFormatException var4) {
+                throw DataXException.asDataXException(MongoDBReaderErrorCode.ILLEGAL_VALUE, "不合法参数");
+            } catch (Exception var5) {
+                throw DataXException.asDataXException(MongoDBReaderErrorCode.UNEXCEPT_EXCEPTION, "未知异常");
+            }
+        } else {
             throw DataXException.asDataXException(MongoDBReaderErrorCode.ILLEGAL_VALUE, "不合法参数");
-        }
-        try {
-            return new MongoClient(parseServerAddress(addressList));
-        } catch (UnknownHostException e) {
-            throw DataXException.asDataXException(MongoDBReaderErrorCode.ILLEGAL_ADDRESS, "不合法的地址");
-        } catch (NumberFormatException e) {
-            throw DataXException.asDataXException(MongoDBReaderErrorCode.ILLEGAL_VALUE, "不合法参数");
-        } catch (Exception e) {
-            throw DataXException.asDataXException(MongoDBReaderErrorCode.UNEXCEPT_EXCEPTION, "未知异常");
         }
     }
 
     public static MongoClient initCredentialMongoClient(Configuration conf, String userName, String password, String database) {
-
-        List<Object> addressList = conf.getList(KeyConstant.MONGO_ADDRESS);
+        List addressList = conf.getList(KeyConstant.MONGO_ADDRESS);
         if (!isHostPortPattern(addressList)) {
             throw DataXException.asDataXException(MongoDBReaderErrorCode.ILLEGAL_VALUE, "不合法参数");
-        }
-        try {
-            MongoCredential credential = MongoCredential.createCredential(userName, database, password.toCharArray());
-            MongoClient mongoClient = new MongoClient(parseServerAddress(addressList), Arrays.asList(credential));
+        } else {
+            try {
 
-            // 设置优先读取副本
-            mongoClient.setReadPreference(com.mongodb.ReadPreference.secondaryPreferred());
+                String address = org.apache.commons.lang3.StringUtils.join(addressList.toArray(), ",");
+                MongoClient mongoClient;
+                String uri;
 
-            return mongoClient;
+                try {
+                    // 优先走默认方式创建MongoClient
+                    MongoCredential credential1 = MongoCredential.createCredential(userName, database, password.toCharArray());
+                    mongoClient = new MongoClient(parseServerAddress(addressList), Arrays.asList(new MongoCredential[]{credential1}));
+                    Document document = mongoClient.getDatabase(database).runCommand(new Document("ping", 1));
 
-        } catch (UnknownHostException e) {
-            throw DataXException.asDataXException(MongoDBReaderErrorCode.ILLEGAL_ADDRESS, "不合法的地址");
-        } catch (NumberFormatException e) {
-            throw DataXException.asDataXException(MongoDBReaderErrorCode.ILLEGAL_VALUE, "不合法参数");
-        } catch (Exception e) {
-            throw DataXException.asDataXException(MongoDBReaderErrorCode.UNEXCEPT_EXCEPTION, "未知异常");
+                } catch (Exception e) {
+                    mongoClient = null;
+                }
+                if (null == mongoClient) {
+                    uri = "mongodb://" + userName + ":" + password + "@" + address + "/?authMechanism=SCRAM-SHA-1";
+                    MongoClientURI mongoClientURI = new MongoClientURI(uri);
+                    mongoClient = new MongoClient(mongoClientURI);
+                    Document document = mongoClient.getDatabase(database).runCommand(new Document("ping", 1));
+                }
+                // 设置优先读取副本
+                mongoClient.setReadPreference(com.mongodb.ReadPreference.secondaryPreferred());
+                return mongoClient;
+
+            } catch (NumberFormatException var15) {
+                throw DataXException.asDataXException(MongoDBReaderErrorCode.ILLEGAL_VALUE, "不合法参数");
+            } catch (Exception var16) {
+                throw DataXException.asDataXException(MongoDBReaderErrorCode.UNEXCEPT_EXCEPTION, "未知异常");
+            }
         }
     }
 
@@ -67,14 +82,21 @@ public class MongoUtil {
      * @param addressList
      * @return
      */
-    private static boolean isHostPortPattern(List<Object> addressList) {
-        for (Object address : addressList) {
-            String regex = "(\\S+):([0-9]+)";
-            if (!((String) address).matches(regex)) {
-                return false;
+    private static boolean isHostPortPattern(List addressList) {
+        Iterator var1 = addressList.iterator();
+
+        Object address;
+        String regex;
+        do {
+            if (!var1.hasNext()) {
+                return true;
             }
-        }
-        return true;
+
+            address = var1.next();
+            regex = "(\\S+):([0-9]+)";
+        } while (((String) address).matches(regex));
+
+        return false;
     }
 
     /**
@@ -83,17 +105,18 @@ public class MongoUtil {
      * @param rawAddressList
      * @return
      */
-    private static List<ServerAddress> parseServerAddress(List<Object> rawAddressList) throws UnknownHostException {
-        List<ServerAddress> addressList = new ArrayList<ServerAddress>();
+    private static List parseServerAddress(List<Object> rawAddressList) throws UnknownHostException {
+        ArrayList addressList = new ArrayList();
         for (Object address : rawAddressList) {
             String[] tempAddress = ((String) address).split(":");
-            try {
-                ServerAddress sa = new ServerAddress(tempAddress[0], Integer.valueOf(tempAddress[1]));
-                addressList.add(sa);
-            } catch (Exception e) {
-                throw new UnknownHostException();
+            for (String temp : tempAddress) {
+                LOG.debug("address {},temp {}", address, temp);
             }
+            ServerAddress e = new ServerAddress(tempAddress[0], Integer.parseInt(tempAddress[1]));
+            addressList.add(e);
         }
+
+
         return addressList;
     }
 }
